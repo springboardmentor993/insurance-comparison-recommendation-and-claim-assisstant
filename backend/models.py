ea1ac43dcb661bf656ee4b34a383e24f6adf4f95
Chandra Sekhar Sadhu <chandrasekharsadhu79@gmail.com>
2026-02-18 09:37:34 +0530

    feat: add UserPolicies, Claims, and ClaimDocuments models with corresponding SQL schemas

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,10 @@
-from sqlalchemy import Column, Integer, String, Numeric, DateTime, ForeignKey, Date, Text
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, ForeignKey, Date, Text, Boolean, TIMESTAMP
 from sqlalchemy.dialects.postgresql import JSONB
 from datetime import datetime
 from database import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
 
 
 class User(Base):
@@ -39,12 +42,46 @@ class Recommendation(Base):
     reason = Column(Text)
     created_at = Column(DateTime, default=datetime.utcnow)
 
-# class RecommendedPolicy(Base):
-#     __tablename__ = "recommended_policies"
+class UserPolicies(Base):
+    __tablename__ = "userpolicies"
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-#     policy_id = Column(Integer, ForeignKey("policies.id"), nullable=False)
-#     score = Column(float, nullable=False)
-#     reason = Column(String, nullable=False)
-#     recommended_at = Column(DateTime, default=datetime.utcnow)
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    policy_id = Column(Integer, ForeignKey("policies.id", ondelete="CASCADE"), nullable=False)
+    policy_number = Column(String(50), unique=True, nullable=False)
+    start_date = Column(Date, nullable=False)
+    end_date = Column(Date, nullable=False)
+    premium = Column(Numeric(12, 2), nullable=False)
+    status = Column(String(20), default="active")
+    auto_renew = Column(Boolean, default=False)
+
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+
+class Claims(Base):
+    __tablename__ = "claims"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_policy_id = Column(Integer, ForeignKey("userpolicies.id", ondelete="CASCADE"), nullable=False)
+    claim_number = Column(String(50), unique=True, nullable=False)
+    claim_type = Column(String(100), nullable=False)
+    incident_date = Column(Date, nullable=False)
+    amount_claimed = Column(Numeric(12, 2), nullable=False)
+    status = Column(String(20), default="draft")
+
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+    user_policy = relationship("UserPolicies")
+
+
+class ClaimDocuments(Base):
+    __tablename__ = "claimdocuments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    claim_id = Column(Integer, ForeignKey("claims.id", ondelete="CASCADE"), nullable=False)
+    file_url = Column(String, nullable=False)
+    doc_type = Column(String(100))
+
+    uploaded_at = Column(TIMESTAMP, server_default=func.now())
+
+    claim = relationship("Claims")
